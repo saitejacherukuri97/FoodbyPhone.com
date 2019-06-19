8d36112072df3fcb6877bacb6a6264c151b1f581
Sai Teja Cherukuri <saitejacherukuri97@gmail.com>
2019-06-19 23:59:52 +0530

    Initial commit

---
+++ js/loginregis.js
@@ -0,0 +1,13 @@
+function success() {
+   alert('Successfully registered');
+}
+
+function validate() {
+  var x = document.getElementById('em');
+  var y = document.getElementById('passw');
+  var x1 = document.getElementById('email');
+  var y1 = document.getElementById('pass');
+  if(x.value == x1.value && y.value==y1.value){
+     
+  }
+}
